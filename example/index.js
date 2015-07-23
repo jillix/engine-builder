bd2f2e9754012e26b0532f85632ebe223facbbb3
Ionică Bizău <bizauionica@gmail.com>
2015-07-23 12:59:12 +0300

    Fetch the composition in the example file

--- example/index.js
+++ example/index.js
@@ -1,6 +1,17 @@
 // Dependencies
-var Parser = require("../lib");
+var Parser = require("../lib")
+  , EngineTools = require("engine-tools")
+  ;
 
-Parser("service-dev", function (err, data) {
-    console.log(err, data);
+// Get the composition
+EngineTools.getComposition("service-dev", { iName: true }, function (err, data) {
+
+    if (err) {
+        return console.log("Failed to get the composition.");
+    }
+
+    // Parse it
+    Parser(data, {}, {}, function (err, data) {
+        console.log(err, data);
+    });
 });
