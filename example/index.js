0fbf906403d796bfcdfd9bad34e61eaa8a416be9
Ionică Bizău <bizauionica@gmail.com>
2015-12-15 17:37:41 +0200

    Cleaning up the things

--- example/index.js
+++ example/index.js
@@ -5,21 +5,3 @@ var Parser = require("../lib")
   , SameTime = require("same-time")
   ;
 
-// Constants
-const APP = "service-dev";
-
-SameTime([
-    // Instances
-    EngineTools.getComposition.bind(EngineTools, APP, { iName: true })
-    // Get service file
-  , EngineTools.getService.bind(EngineTools, APP)
-  , EngineTools.getModuleInfo.bind(EngineTools, APP)
-], function (err, data) {
-    if (err) { return console.error(err); }
-    Parser(data[0], data[1], data[2], {
-        handleServer: false
-      , parseLines: false
-    }, function (err, data) {
-        console.log(err, data.prepare());
-    });
-});
