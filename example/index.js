685324a0728d613e9388c5b5986db4552714b500
Ionică Bizău <bizauionica@gmail.com>
2015-12-20 11:31:49 +0200

    Stringify the results

--- example/index.js
+++ example/index.js
@@ -3,5 +3,5 @@ const EngineBuilder = require("../lib");
 
 var eb = new EngineBuilder(`${__dirname}/engine-test`);
 eb.getGraph(function (err, data) {
-    console.log(err || data);
+    console.log(err || JSON.stringify(data, null, 4));
 });
